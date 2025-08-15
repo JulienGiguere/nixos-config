d75597b05987896def0abd3bd255bd46e7afc1ec
installer <installer@gmail.com>
2025-08-15 00:35:27 -0400

    waybar

--- modules/core/services.nix
+++ modules/core/services.nix
@@ -13,8 +13,8 @@
       enable = true;
       ports = [ 22 ];
       settings = {
-        PasswordAuthentication = true;
-        AllowUsers = null; # Allows all users by default. Can be [ "user1" "user2" ]
+        PasswordAuthentication = false;
+        AllowUsers = ["julien"]; # Allows all users by default. Can be [ "user1" "user2" ]
         UseDns = true;
         X11Forwarding = false;
         PermitRootLogin = "prohibit-password";
--- modules/home/hyprland/hyprlock.nix
+++ modules/home/hyprland/hyprlock.nix
@@ -1,100 +1,186 @@
 { pkgs, ... }:
-let 
-  text = "rgb(251, 241, 199)";
-in
 {
-  home.packages = [ pkgs.hyprlock ];
-  xdg.configFile."hypr/hyprlock.conf".text = ''
-    # BACKGROUND
-    background {
-      monitor =
-      path = ${../../../wallpapers/otherWallpaper/gruvbox/forest.jpg}
-      blur_passes = 1
-      contrast = 0.8916
-      brightness = 0.8172
-      vibrancy = 0.1696
-      vibrancy_darkness = 0.0
-    }
+  programs.hyprlock = {
+    enable = true;
+    settings = {
+      # source = $HOME/.config/hypr/wallust/wallust-hyprland.conf
+      # $Scripts = $HOME/.config/hypr/scripts
 
-    # GENERAL
-    general {
-      hide_cursor = true
-      no_fade_in = false
-      grace = 0
-      disable_loading_bar = false
-    }
+      general =  {
+        grace = 1;
+        fractional_scaling = 2;
+        immediate_render = true;
+      };
 
-    # Time
-    label {
-      monitor = 
-      text = cmd[update:1000] echo "$(date +"%k:%M")"
-      color = rgba(235, 219, 178, .9)
-      font_size = 111
-      font_family = JetBrainsMono NF Bold
-      position = 0, 270
-      halign = center
-      valign = center
-    }
+      background = {
+        path = "/home/julien/Pictures/wallpapers/wallpaper.jpg" ;
 
-    # Day
-    label {
-      monitor =
-      text = cmd[update:1000] echo "- $(date +"%A, %B %d") -"
-      color = rgba(235, 219, 178, .9)
-      font_size = 20
-      font_family = CaskaydiaCove Nerd Font
-      position = 0, 160
-      halign = center
-      valign = center
-    }
+        color = "rgb(0,0,0)";
+
+        blur_size = 3;
+        blur_passes = 2;
+        noise = 0.0117;
+        contrast = 1.3000;
+        brightness = 0.8000;
+        vibrancy = 0.2100;
+        vibrancy_darkness = 0.0;
+      };
 
 
-    # USER-BOX
-    shape {
-      monitor =
-      size = 350, 50
-      color = rgba(225, 225, 225, .2)
-      rounding = 15
-      border_size = 0
-      border_color = rgba(255, 255, 255, 0)
-      rotate = 0
+      # # Date
+      #     label = {
+      #       text = "cmd[update:18000000] echo '<b>' '$(date +'%A, %-d %B')' '</b>';
+      #       color = #F68D3E;
+      #       font_size = 16;
+      #       font_family = Victor Mono Bold Italic;
+      #       position = 0, -120;
+      #       halign = center;
+      #       valign = center;
+      #   };
 
-      position = 0, -230
-      halign = center
-      valign = center
-    }
+      # Hour-Time
+      # label = {
+      #     text = cmd[update:1000] echo "$(date +"%H")"
+      # #    text = cmd[update:1000] echo "$(date +"%I")" #AM/PM
+      # 	#color = rgba(255, 185, 0, .8)
+      #     color = #F68D3E
+      #     font_size = 200
+      #     font_family = JetBrainsMono Nerd Font ExtraBold
+      #     position = 0, -60
+      #     halign = center
+      #     valign = top
+      # }
 
-    # USER
-    label {
-      monitor =
-      text =   $USER
-      color = rgba(235, 219, 178, .9)
-      font_size = 16
-      font_family = CaskaydiaCove Nerd Font
-      position = 0, -230
-      halign = center
-      valign = center
-    }
+      # # Minute-Time
+      # label {
+      #     monitor =
+      #     text = cmd[update:1000] echo "$(date +"%M")"
+      #     #color = rgba(15, 10, 222, .8)
+      #     color = $color12
+      #     font_size = 200
+      #     font_family = JetBrainsMono Nerd Font ExtraBold
+      #     position = 0, -340
+      #     halign = center
+      #     valign = top
+      # }
 
-    # INPUT FIELD
-    input-field {
-      monitor =
-      size = 350, 50
-      outline_thickness = 0
-      rounding = 15
-      dots_size = 0.25 # Scale of input-field height, 0.2 - 0.8
-      dots_spacing = 0.4 # Scale of dots' absolute size, 0.0 - 1.0
-      dots_center = true
-      outer_color = rgba(255, 255, 255, 0)
-      inner_color = rgba(225, 225, 225, 0.2)
-      color = rgba(235, 219, 178, .9)
-      font_color = rgba(235, 219, 178, .9)
-      fade_on_empty = false
-      placeholder_text = <i><span foreground="##ebdbb2e5">Enter Password</span></i>
-      hide_input = false
-      position = 0, -300
-      halign = center
-      valign = center
-    }
-  '';
+      # # Seconds-Time
+      # label {
+      #     monitor =
+      #     text = cmd[update:1000] echo "$(date +"%S")"
+      # #    text = cmd[update:1000] echo "$(date +"%S %p")" #AM/PM
+      #     color = $color11
+      #     font_size = 35
+      #     font_family = JetBrainsMono Nerd Font ExtraBold
+      #     position = 0, -350
+      #     halign = center
+      #     valign = top
+      # }
+
+      # Put a picture of choice here. Default is the current wallpaper
+      #image {
+      #    monitor =
+      #    path = $HOME/.config/hypr/wallpaper_effects/.wallpaper_current
+      #    size = 160
+      #    rounding = -1
+      #    border_size = 0
+      #    border_color = $color11
+      #    rotate = 0
+      #    reload_time = -1
+      #    position = 0, 280
+      #    halign = center
+      #    valign = bottom
+      #}
+
+      # USER
+      label = [
+        {
+          text = "  $USER";
+          color = "#F68D3E";
+          font_size = "24";
+          font_family = "Victor Mono Bold Oblique";
+          position = "0, 220";
+          halign = "center";
+          valign = "bottom";
+        }
+        # battery information
+        {
+          text = "cmd[update:1000] echo '<b> '$($Scripts/Battery.sh)' </b>'";
+          color = "#F68D3E";
+          font_size = 16;
+          font_family = "Victor Mono Bold Oblique";
+          position = "0, 30";
+          halign = "right";
+          valign = "bottom";
+        }
+        # uptime
+        {
+          text = "cmd[update:60000] echo '<b> '$(uptime -p || $Scripts/UptimeNixOS.sh)' </b>'";
+          color = "#F68D3E";
+          font_size = 16;
+          font_family = "Victor Mono Bold Oblique";
+          position = "0, 0";
+          halign = "right";
+          valign = "bottom";
+        }
+        # Date
+        {
+          text = "cmd[update:18000000] echo '<b>' '$(date +'%A, %-d %B')' '</b>'";
+          color = "#F68D3E";
+          font_size = 16;
+          font_family = "Victor Mono Bold Italic";
+          position = "0, -120";
+          halign = "center";
+          valign = "center";
+        }
+    ];
+
+
+      # # Keyboard LAYOUT
+      # label {
+      #     monitor =
+      #     text = $LAYOUT
+      #     color = #F68D3E
+      #     font_size = 10
+      #     font_family = Victor Mono Bold Oblique
+      #     position = 0, 70
+      #     halign = center
+      #     valign = bottom
+      # }
+
+      # # weather edit the scripts for locations
+      # # weather scripts are located in ~/.config/hypr/UserScripts Weather.sh and/or Weather.py
+      # # see https://github.com/JaKooLit/Hyprland-Dots/wiki/TIPS#%EF%B8%8F-weather-app-related-for-waybar-and-hyprlock
+      # label {
+      #     monitor =
+      #     text = cmd[update:3600000] [ -f "$HOME/.cache/.weather_cache" ] && cat "$HOME/.cache/.weather_cache"
+      #     color = #F68D3E
+      #     font_size = 16
+      #     font_family = Victor Mono Bold Oblique
+      #     position = 50, 0
+      #     halign = left
+      #     valign = bottom
+      # }
+
+      # INPUT FIELD
+      input-field = {
+        size = "200, 60";
+        outline_thickness = 2;
+        dots_size = 0.2; # Scale of input-field height, 0.2 - 0.8
+        dots_spacing = 0.2; # Scale of dots' absolute size, 0.0 - 1.0
+        dots_center = true;
+        outer_color = "$color11";
+        inner_color = "rgba(255, 255, 255, 0.1)";
+        capslock_color = "rgb(255,255,255)";
+        font_color = "#F68D3E";
+        fade_on_empty = false;
+        font_family = "Victor Mono Bold Oblique";
+        placeholder_text = "<i><span foreground='##ffffff99'>🔒 Type Password</span></i>";
+        hide_input = false;
+        position = "0, 100";
+        halign = "center";
+        valign = "bottom";
+      };
+    };
+  };
 }
